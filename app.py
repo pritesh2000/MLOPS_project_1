80d326d2b6be65f3ffcbddaa19b1265649f180ab
pritesh2000 <priteshptadvi29@gmail.com>
2024-03-11 21:55:23 +0530

    debug disabled

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def index():
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=7000, debug=True)  
-    app.run(host="0.0.0.0", port=7000, debug=True)
+    app.run(host="0.0.0.0", port=7000)
