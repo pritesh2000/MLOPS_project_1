8c3545f51430f83f78859fc65d3e09214c46c5d8
pritesh2000 <priteshptadvi29@gmail.com>
2024-03-15 15:42:37 +0530

    checking why yellow button doesn't show up

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def index():
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=7000, debug=True)  
+    # app.run(host="0.0.0.0", port=8080, debug=True)  
     app.run(host="0.0.0.0", port=8080)
